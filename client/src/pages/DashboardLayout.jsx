d4ede7ee5b3fdbaf00c2169ea5ec10a74220a349
Ahmed Shaker <ashaker6573@gmail.com>
2023-10-27 13:26:28 +0200

    first commit

---
+++ client/src/pages/DashboardLayout.jsx
@@ -0,0 +1,72 @@
+import { Outlet, redirect, useLoaderData, useNavigate } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/Dashboard';
+import { Navbar, BigSidebar, SmallSidebar } from '../components';
+import { checkDefaultTheme } from '../App';
+import { useState, createContext, useContext } from 'react';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const loader = async () => {
+  try {
+    const { data } = await customFetch.get('/users/current-user');
+    return data;
+  } catch (error) {
+    return redirect('/');
+  }
+};
+const DashboardContext = createContext();
+
+const Dashboard = () => {
+  const { user } = useLoaderData();
+  const navigate = useNavigate();
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme);
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle('dark-theme', newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
+  };
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const logoutUser = async () => {
+    navigate('/');
+    await customFetch.get('/auth/logout');
+    toast.success('Logging out...');
+  };
+  return (
+    <DashboardContext.Provider
+      value={{
+        user,
+        showSidebar,
+        isDarkTheme,
+        toggleDarkTheme,
+        toggleSidebar,
+        logoutUser,
+      }}
+    >
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet context={{ user }} />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </DashboardContext.Provider>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useDashboardContext = () => useContext(DashboardContext);
+
+export default Dashboard;
